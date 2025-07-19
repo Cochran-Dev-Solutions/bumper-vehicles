f95ede18d1a728e3ac4efd02d9d1792d9a6484d0
Nathan Cochran <58408117+nacochran@users.noreply.github.com>
2025-07-19 15:14:07 -0500

    Fixed Imports.

--- apps/game-frontend/src/EventObjects/InputManager.js
+++
@@ -1,8 +0,0 @@
-// client/src/EventObjects/InputManager.js
-
-export default class InputManager {
-  constructor() {
-    this.events = [];
-    this.current = {};
-  }
-}
--- apps/game-frontend/src/GameActors/BlockActor.js
+++
@@ -1,8 +0,0 @@
-import StaticActor from "./StaticActor.js";
-
-export class BlockActor extends StaticActor {
-  constructor(config) {
-    super(config);
-    this.imageNames.push("Block/block.png"); // Add block image to load
-  }
-} 
